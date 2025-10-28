37c8c901370a0b0b78c07818983def831b049726
Saud-Al5 <sa8429@nyu.edu>
2025-10-28 16:02:20 -0400

    Added package skeleton and test folder

---
+++ joytide/__main__.py
@@ -0,0 +1,7 @@
+def main():
+    print("joytide package is installed and running")
+    print("functions will be added by team_riptide")
+
+
+if __name__ == "__main__":
+    main()
---
+++ joytide/core.py
@@ -0,0 +1,11 @@
+"""
+core module for joytide
+
+we will add our function here in our own branch.
+after finishing a function we will need to add tests for them in the tests folder
+
+for now this file is empty on purpose.
+"""
+
+# placeholder so the file is valid python
+pass
