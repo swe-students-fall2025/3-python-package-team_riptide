fabc27139a45b6b79226f5f03f8be9d6dde437d7
ohortig <oh2065@nyu.edu>
2025-11-04 13:53:56 -0500

    Update package version

--- joytide/__init__.py
+++ joytide/__init__.py
@@ -5,7 +5,7 @@ team_riptide
 
 from .core import banner, confetti, game_2048, art
 
-__version__ = "0.0.2"
+__version__ = "0.0.4"
 
 # will be filled as we add functions
 __all__ = ["banner", "confetti", "game_2048", "art"]
